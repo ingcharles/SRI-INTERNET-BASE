bf091c8b15091c83ba37756bfac87102d339dca8
Carlos Anchundia <ceanchundia@sri.gob.ec>
2025-10-08 10:33:36 -0500

    base 20

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+declare module 'lucide-vue-next'
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  // Usar tipos estrictos para evitar any/{} vacíos
+  const component: DefineComponent<Record<string, never>, Record<string, never>, unknown>
+  export default component
+}
--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,12 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@interfaces': fileURLToPath(new URL('./src/interfaces', import.meta.url)),
     },
   },
 })
